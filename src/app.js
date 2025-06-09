6dffa781a741f7e571e910a0e2a040194f3753a7
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-06-09 21:46:27 +0530

    testing get and post call through postmen

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 
 const app = express();
+// this will only get call to /user
+app.get("/user");
 
-app.use((req, res) => {
-    res.send("Hello server");
-});
 
-app.use("/", (req, res) => {
+app.use("/test", (req, res) => {
     res.send("Namaste from the dashBoard");
 });
 
-app.listen(3000, () => {
-    console.log("Server is successfully running on port 3000...");
+app.listen(7777, () => {
+    console.log("Server is successfully running on port 7777...");
 });
+// start 42 min
