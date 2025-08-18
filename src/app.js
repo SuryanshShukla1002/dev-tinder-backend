f92eeabd49d4065e78f5f1fb11f74845a07d2774
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-08-19 02:45:48 +0530

    added auth profile and requestRouter

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const connectDB = require("./config/database");
 const app = express();
-const User = require("./models/user");
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.post("/signup", async (req, res) => {
-    const user = new User(req.body);
-    // Creating a new instance of the user model
-    await user.save();
-    res.send("User Added Successfully!");
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
 
-});
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
 
 connectDB().then(() => {
     console.log("Database connection is successfull");
@@ -21,6 +22,5 @@ connectDB().then(() => {
 }).catch((error) => {
     console.log("Databse cannot be connected!");
 });
-
 
 
