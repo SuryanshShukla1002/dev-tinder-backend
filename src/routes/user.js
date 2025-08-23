799301aa7d3ae51af56211aff50f33100f9ef320
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-08-23 20:17:46 +0530

    Fix connections API to return correct user data

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { userAuth } = require('../middlewares/auth');
-const ConnectionRequest = require('../models/connectionRequest');
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
 
 const USER_SAVE_DATA = "firstName lastName age gender about skills";
@@ -11,12 +11,13 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
 
         const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
-            status: "interested"
+            status: "interested",
         }).populate("fromUserId", USER_SAVE_DATA);
 
-
-
-        res.json({ message: "Data fetched Successfully: ", data: connectionRequests });
+        res.json({
+            message: "Data fetched Successfully: ",
+            data: connectionRequests,
+        });
     } catch (err) {
         res.status(404).send("ERROR " + err.message);
     }
@@ -28,11 +29,18 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         const connectionRequests = await ConnectionRequest.find({
             $or: [
                 { toUserId: loggedInUser._id, status: "accepeted" },
-                { fromUserId: loggedInUser._id, status: "accepeted" }
-            ]
-        }).populate("fromUserId", USER_SAVE_DATA);
+                { fromUserId: loggedInUser._id, status: "accepeted" },
+            ],
+        })
+            .populate("fromUserId", USER_SAVE_DATA)
+            .populate("toUserId", USER_SAVE_DATA);
 
-        const data = connectionRequests.map((row) => row.fromUserId);
+        const data = connectionRequests.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
 
         res.json({ data });
     } catch (err) {
