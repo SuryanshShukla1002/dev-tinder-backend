f3221a9f342e9786751f4322bb11efeae9db7b55
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-09-02 11:09:35 +0530

    framed the error

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -7,7 +7,7 @@ const userAuth = async (req, res, next) => {
 
         const { token } = req.cookies;
         if (!token) {
-            throw new Error("Token is not valid!!!!!!!!!!!!!!!");
+            return res.status(401).send("Please login!");
         }
 
         const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
@@ -18,7 +18,7 @@ const userAuth = async (req, res, next) => {
         if (!user) {
             throw new Error("User not found");
         }
-        req.user = user
+        req.user = user;
         next();
     } catch (err) {
         res.status(400).send("Error: " + err.message);
