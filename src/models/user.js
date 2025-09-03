0f00418de0c790e991b4afe88db753c6188afa3f
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-09-03 22:27:57 +0530

    define mongoose user schema with auth methods

--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,92 @@
 const mongoose = require('mongoose');
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid email address: " + value);
+            }
+        }
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error("Enter a strong password: " + value);
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18
     },
     gender: {
-        type: String
+        type: String,
+        enum: {
+            values: ["male", "female", "others"],
+            message: `{VALUE} is not a valid gender type`
+        },
+        validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("Gender data is not valid");
+            }
+        }
+
     },
-});
+    photoUrl: {
+        type: String,
+        default: "https://png.pngtree.com/png-vector/20230313/ourmid/pngtree-female-admin-and-assistant-job-vacancies-vector-png-image_6646509.png",
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("Invalid PhotoUrl address: " + value);
+            }
+        }
+
+    },
+    about: {
+        type: String,
+        default: "This is a default about of the user"
+    },
+    skills: {
+        type: [String],
+
+    }
+}, { timestamps: true });
+
+
+
+userSchema.methods.getJWT = async function () {
+    const user = this;
+
+    const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790", { expiresIn: "7d" });
+
+    return token;
+};
+
+userSchema.methods.validatePassword = async function (passwordInputByUser) {
+    const user = this;
+    const passwordHash = user.password;
+    const isPasswordValid = await bcrypt.compare(passwordInputByUser, passwordHash);
+
+    return isPasswordValid;
+};
 
 
 module.exports = mongoose.model("User", userSchema);;
