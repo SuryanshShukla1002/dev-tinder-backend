deedfea2e6b0e8b243ffea7d9b9eb6901f5838ab
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-09-28 16:55:43 +0530

    socket io server setup

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const http = require("http");
 require('dotenv').config();
+
 
 
 app.use(cors({
@@ -18,6 +20,7 @@ const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require('./routes/user');
 const paymentRouter = require('./routes/payment');
+const initializeSocket = require('./utils/socket');
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
@@ -25,9 +28,13 @@ app.use("/", requestRouter);
 app.use("/", userRouter);
 app.use("/", paymentRouter);
 
+const server = http.createServer(app);
+initializeSocket(server);
+
+
 connectDB().then(() => {
     console.log("Database connection is successfull");
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
         console.log("Server is successfully running on port 7777...");
     });
 }).catch((error) => {
