242a392e56cfcfb30ac52c87e79dc924d668a015
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-09-29 10:39:08 +0530

    Add function to generate secret chat room ID

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,31 +1,30 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+const getSecrectRoomId = (userId, targetUserId) => {
+    return crypto
+        .createHash("sha256")
+        .update([userId, targetUserId].sort().join("_"))
+        .digest("hex");
+};
 
 const initializeSocket = (server) => {
-
     const io = socket(server, {
         cors: {
-            origin: "http://localhost:5173"
-        }
+            origin: "http://localhost:5173",
+        },
     });
     io.on("connection", (socket) => {
         socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
-            const roomId = [userId, targetUserId].sort().join("_");
+            const roomId = getSecrectRoomId(userId, targetUserId);
             console.log("Joining Room: " + roomId);
             socket.join(roomId);
         });
-        socket.on("sendMessage", ({
-            firstName,
-            userId,
-            targetUserId,
-            text,
-        }) => {
-            const roomId = [userId, targetUserId].sort().join("_");
+        socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+            const roomId = getSecrectRoomId(userId, targetUserId);
             console.log(firstName + " " + text);
             io.to(roomId).emit("messageReceived", { firstName, text });
         });
-        socket.on("disconnect", () => {
-
-        });
+        socket.on("disconnect", () => { });
     });
 };
 
